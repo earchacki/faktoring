8007eb1eba8f13729c6b8daffdd8f4cdb813bf72
earchacki <80483935+earchacki@users.noreply.github.com>
2022-07-04 16:19:18 +0200

    Update contractors.py

--- tests/page_object/contractors.py
+++ tests/page_object/contractors.py
@@ -1,7 +1,6 @@
 from tests.helpers.support_functions import *
 from tests.page_object import main_page
 from config import test_settings
-from time import sleep
 
 search_input = '/html/body/app-root/app-layout-authenticated/main/div/app-contractor-list/app-base-list/div[2]/div/app-base-filter-inline/div/div/div[1]/input'
 search_button = '/html/body/app-root/app-layout-authenticated/main/div/app-contractor-list/app-base-list/div[2]/div/app-base-filter-inline/div/div/div[1]/div/button'
@@ -19,7 +18,6 @@ def search_contractor(driver_instance):
     elem.send_keys(test_settings.documents.contractor)
     elem1 = driver_instance.find_element(By.XPATH, search_button)
     elem1.click()
-    sleep(1)
 
 
 def check_contractor_visible(driver_instance):
